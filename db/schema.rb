61aa306ace9d430f17c1cd54f78c13edd6e04e89
Brandon Clapp <clapp09@gmail.com>
2015-06-22 20:26:49 -0500

    Added name property to devise model and modified new and edit forms to account for it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622035731) do
+ActiveRecord::Schema.define(version: 20150622234716) do
 
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150622035731) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
