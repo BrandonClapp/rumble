93091478ecfeef1f2b67c1ba339378d28960147d
Brandon Clapp <clapp09@gmail.com>
2015-06-23 07:58:53 -0500

    user profiles associated with each user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+#before_filter :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+        format.html # show.html.erb
+        format.xml { render :xml => @user }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'teams#index'
   resources :teams
+  resources :users
 end
