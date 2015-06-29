036ea58a882b23ed5fb2be0ff0046419c7b92cb8
Brandon Clapp <clapp09@gmail.com>
2015-06-29 07:51:42 -0500

    showing teams page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,6 @@ class ApplicationController < ActionController::Base
         |u| u.permit(registration_params) 
       }
     end
-
-    # devise_parameter_sanitizer.for(:sign_up) << :name
-    # devise_parameter_sanitizer.for(:account_update) << :name
   end
 
 end
