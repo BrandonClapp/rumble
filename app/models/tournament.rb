b79f5d967691b5acd19f58bf9689584a437908ef
Brandon Clapp <clapp09@gmail.com>
2015-07-01 18:42:12 -0500

    tournament form enhancements and validation

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,8 @@
 class Tournament < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :team_size, presence: true;
+
   enum tournament_type: { ladder: 0, round_robin: 1 }
   belongs_to :league
 end
