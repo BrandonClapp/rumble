5f6db6557a6579176c29b352c6cce383b9eda6dc
Brandon Clapp <clapp09@gmail.com>
2015-07-02 09:15:27 -0500

    added association between teams and matches but can't figure out the proper way to set up activerecord foreign key relationship on the matches table

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,17 @@ class Team < ActiveRecord::Base
   has_many :users, through: :memberships
 
   has_and_belongs_to_many :tournaments
+
+  def initiated_matches
+    Match.where('initiating_team_id = ?', self.id)
+  end
+
+  def defended_matches
+    Match.where('defending_team_id = ?', self.id)
+  end
+
+  def matches 
+    initiated_matches + defended_matches
+  end
+
 end
